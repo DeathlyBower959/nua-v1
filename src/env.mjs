1e934332e47feff92ae735c1565dd4b301f79cd4
DeathlyBower959 <DeathlyBower959yt@gmail.com>
2023-09-20 21:47:32 -0400

    Vercel env

--- src/env.mjs
+++ src/env.mjs
@@ -1,15 +1,15 @@
 import { createEnv } from '@t3-oss/env-nextjs';
-import { z } from 'zod';
+// import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    SERVER_VAR: z.string(),
+    // SERVER_VAR: z.string(),
   },
   client: {
-    NEXT_PUBLIC_CLIENT_VAR: z.coerce.number(),
+    // NEXT_PUBLIC_CLIENT_VAR: z.coerce.number(),
   },
   runtimeEnv: {
-    SERVER_VAR: process.env.SERVER_VAR,
-    NEXT_PUBLIC_CLIENT_VAR: process.env.CLIENT_VAR,
+    // SERVER_VAR: process.env.SERVER_VAR,
+    // NEXT_PUBLIC_CLIENT_VAR: process.env.CLIENT_VAR,
   },
 });
